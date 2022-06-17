c7998611b167c862c18ad21bdd7c057988b11ab8
Thiago Fernandes <falecomobranco@yahoo.com.br>
2022-06-17 10:13:29 -0300

    html cabecalho

--- diana-costura-criativa/src/pages/Produtos/index.tsx
+++ diana-costura-criativa/src/pages/Produtos/index.tsx
@@ -1,4 +1,5 @@
 import styles from "./Produtos.module.scss";
+import {ReactComponent as Logo} from "assets/logo-preto.svg"
 
 export default function Produtos() {
   return (
@@ -20,7 +21,38 @@ export default function Produtos() {
       </div>
 
       {/*cabecalho */}
-      
+      <section>
+        <div>
+          <Logo />
+          <nav>
+            <ul>
+              <li><a href="#">Home</a></li>
+              <li><a href="#">Sobre</a></li>
+              <li><a href="#">Produtos</a></li>
+              <li><a href="#">Contato</a></li>
+            </ul>
+          </nav>
+        </div>
+
+        <div>
+          {/*container do titulo principal e botao */}
+          <div>
+            <div>
+              <p>Feitos à mão com</p>
+              <p>Paixão</p>
+            </div>
+            <div>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vel corporis ducimus tempora reiciendis error, quaerat totam, quam, labore aspernatur beatae nemo.</p>
+              <button>Saiba Mais</button>
+            </div>
+          </div>
+          {/*container da imagem de capa */}
+          <div>
+            <img src="assets/produto-ex.png" alt="Produto" />
+          </div>
+        </div>
+      </section>
+
     </>
   );
 }
