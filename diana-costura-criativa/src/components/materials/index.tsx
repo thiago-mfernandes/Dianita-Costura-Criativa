cda18b9c72ccb2389d2ae837df9f1f2a9d9e4327
Thiago Fernandes <falecomobranco@yahoo.com.br>
2022-06-21 15:46:23 -0300

    secao de materiais estilizada

--- diana-costura-criativa/src/components/materials/index.tsx
+++ diana-costura-criativa/src/components/materials/index.tsx
@@ -1,45 +1,44 @@
-import styles from "Materials.module.scss";
+import styles from "./Materials.module.scss";
+import Linhas from "assets/flaticon/yarn.svg";
 
 export default function Materials() {
   return (
     <>
       {/* meus materiais */}
-      <section className="">
-        <div className="">
-          <h2>Meus Materiais</h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui,
-            temporibus reiciendis exercitationem ullam inventore voluptas sit
-            deleniti dolores quam porro blanditiis alias modi, eum aliquid
-            praesentium, ad eos! Veritatis, quisquam?
+      <section className={styles.materials}>
+        <div className={styles.materials__container}>
+          <h2 className={styles.materials__title}>Meus Materiais</h2>
+          <p className={styles.materials__content}>
+            Todos os itens que compõe cada peça da nossa linha são
+            criteriosamente escolhidos, um a um, para que o acabamento final e
+            qualidade de cada unidade seja o mais alto e belo possível, porque
+            faço com <span>dedicação!</span>
           </p>
         </div>
-        <div>
+
+        <div className={styles.materials__container}>
           {/* card material */}
-          <div>
-            <img src="" alt="ilustracao do material" />
-            <h3>Linhas Premium</h3>
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit,
-              aliquid. In explicabo repudiandae
+          <div className={styles.materials__card}>
+            <img src={Linhas} alt="Ilustração de Linhas" className={styles.materials__card___icon}/>
+            <h3 className={styles.materials__card___title}>Linhas Premium</h3>
+            <p className={styles.materials__card___content}>
+              Linha de costura não é tudo igual! Cada linha tem a sua indicação de uso. Linhas de algodão utilizadas para tecidos de fibras naturais e linhas sintéticas para tecidos sintéticos e naturais! Por aqui, escolhemos sempre o uso mais adequado.
             </p>
           </div>
-          {/* card material */}
-          <div>
-            <img src="" alt="ilustracao do material" />
-            <h3>Linhas Premium</h3>
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit,
-              aliquid. In explicabo repudiandae
+
+          <div className={styles.materials__card}>
+            <img src={Linhas} alt="Ilustração de Linhas" className={styles.materials__card___icon}/>
+            <h3 className={styles.materials__card___title}>Linhas Premium</h3>
+            <p className={styles.materials__card___content}>
+              Linha de costura não é tudo igual! Cada linha tem a sua indicação de uso. Linhas de algodão utilizadas para tecidos de fibras naturais e linhas sintéticas para tecidos sintéticos e naturais! Por aqui, escolhemos sempre o uso mais adequado.
             </p>
           </div>
-          {/* card material */}
-          <div>
-            <img src="" alt="ilustracao do material" />
-            <h3>Linhas Premium</h3>
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit,
-              aliquid. In explicabo repudiandae
+
+          <div className={styles.materials__card}>
+            <img src={Linhas} alt="Ilustração de Linhas" className={styles.materials__card___icon}/>
+            <h3 className={styles.materials__card___title}>Linhas Premium</h3>
+            <p className={styles.materials__card___content}>
+              Linha de costura não é tudo igual! Cada linha tem a sua indicação de uso. Linhas de algodão utilizadas para tecidos de fibras naturais e linhas sintéticas para tecidos sintéticos e naturais! Por aqui, escolhemos sempre o uso mais adequado.
             </p>
           </div>
         </div>
