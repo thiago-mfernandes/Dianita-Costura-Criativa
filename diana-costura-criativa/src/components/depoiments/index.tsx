4f31f044072ac725ebda00f180bb5babdd40b7cc
Thiago Fernandes <falecomobranco@yahoo.com.br>
2022-06-23 16:47:18 -0300

    dados fake no json e carrousel de depoimentos implementado

--- diana-costura-criativa/src/components/depoiments/index.tsx
+++ diana-costura-criativa/src/components/depoiments/index.tsx
@@ -1,39 +1,21 @@
 import styles from "./Depoiments.module.scss";
 import { Slider, SliderProps, Slide } from "./slider";
+import data from "./testimonials.json";
 
+export default function Depoiments() {
+  
+  let windowWidth = window.screen.width;
 
-//parado em 24:00
-
-
-
-export default function Depoiments() { 
-  {/* settings do slider */}
   const settings: SliderProps = {
     spaceBetween: 50,
-    slidesPerView: 3,
-    navigation: true,
-    pagination: {
-      clickable: true
-    }
-  }
-
-  const testimonials = [
-    {
-      text: "O porta-óculos valeu a pena! Além de muito bonito, protege a lente do óculos de riscar dentro da bolsa no dia a dia. Recomendadíssimo..",
-      author: "Maria Gabriela",
-      subTitle: "Jornalista",
+    slidesPerView: windowWidth < 767 ? 1 : 2,
+    navigation: data.testimonials.length >= 3,
+    draggable: data.testimonials.length >= 3,
+    loop: data.testimonials.length >= 1,
+    pagination: data.testimonials.length >= 1 && {
+      clickable: true,
     },
-    {
-      text: "Presenteei minha filha com o kit porta-esmalte. Ela amou o presente!",
-      author: "Leontina Vieira",
-      subTitle: "Secretária",
-    },
-    {
-      text: "A bolsa infantil é de excelente qualidade. Possui forro impermeável e bolso interno com zíper para separar coisas dentro. Muito útil quando preciso levar o lanchinho e a madeira do meu filho! Recomendo a todos..",
-      author: "Thiago Fernandes",
-      subTitle: "Programador",
-    }
-  ]; 
+  };
 
   return (
     <>
@@ -46,52 +28,22 @@ export default function Depoiments() {
         </p>
 
         <Slider settings={settings}>
-          <Slide>
-            <h1>Teste 1</h1>
-          </Slide>
-          <Slide>
-            <h1>Teste 2</h1>
-          </Slide>
-          <Slide>
-            <h1>Teste 3</h1>
-          </Slide>
-          <Slide>
-            <h1>Teste 4</h1>
-          </Slide>
+          {data.testimonials.map((testimonial) => (
+            <Slide key={testimonial.id}>
+              <div className={styles.cardDepoiments}>
+                <article className={styles.cardDepoiments__content}>
+                  {testimonial.text}
+                </article>
+                <h3 className={styles.cardDepoiments__author}>
+                  {testimonial.author}
+                </h3>
+                <p className={styles.cardDepoiments__authorSubtitle}>
+                  {testimonial.subTitle}
+                </p>
+              </div>
+            </Slide>
+          ))}
         </Slider>
-
-        <div>
-          {/* aqui deve ser um article, verificar */}
-          <div className={styles.cardDepoiments}>
-            <article className={styles.cardDepoiments__content}>
-            A bolsa infantil é de excelente qualidade. Possui forro impermeável e bolso interno com zíper para separar coisas dentro. Muito útil quando preciso levar o lanchinho e a madeira do meu filho! Recomendo a todos..
-            </article>
-            <h3 className={styles.cardDepoiments__author}>
-              Thiago Fernandes
-            </h3>
-            <p className={styles.cardDepoiments__authorSubtitle}>
-              Programador
-            </p>
-          </div>
-
-          {/* controlador do slider 
-          
-          <div className={styles.controllerContainer}>
-            <button
-              className={styles.controllerContainer__controller}
-              onClick={() => previousTestimonial()}
-            >
-              <span className="material-symbols-outlined">chevron_left</span>
-            </button>
-            <button
-              className={styles.controllerContainer__controller}
-              onClick={() => nextTestimonial(currentTestimonial)}
-            >
-              <span className="material-symbols-outlined">chevron_right</span>
-            </button>
-          </div>
-          */}
-        </div>
       </section>
     </>
   );
