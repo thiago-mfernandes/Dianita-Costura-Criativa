ce4bf5ca6a25a003aa8fa0f843dedbad447916a9
Thiago Fernandes <falecomobranco@yahoo.com.br>
2022-07-06 10:52:18 -0300

    meus produtos e about ajustados no css

--- diana-costura-criativa/src/components/about/index.tsx
+++ diana-costura-criativa/src/components/about/index.tsx
@@ -8,22 +8,20 @@ export default function About() {
         <div className={styles.about__container}>
           <h2 className={styles.about__title}>Quem Sou</h2>
           <img className={styles.about__img} src={ImgDiana} alt="Foto Diana" />
-          <div className={styles.about__container}>
+          <div className={styles.about__container___contentBox}>
             <p className={styles.about__content}>
               Oi, deixa eu me apresentar...
               <br />
-              <br />
               Meu nome é Diana, a artesã do ateliê
-              <span>`${'DIANITA COSTURA CRIATIVA'}`</span>.
+              <span>{`${' DIANITA COSTURA CRIATIVA'}`}</span>.
               <br />
-              Sou técnica de enfermagem e instrumentadora cirúrgica de formação,
+              - Sou técnica de enfermagem e instrumentadora cirúrgica de formação,
               porém, o artesanal sempre fez parte da minha vida, nas mais
               diferentes técnicas. Em meados de 2018, conheci a costura criativa
               e me encantei, logo tomei coragem, comprei minha primeira máquina
               e foi o início de tudo.
               <br />
-              <br />
-              Fui aprendendo e a cada peça que confeccionava, procurava me
+              - Fui aprendendo e a cada peça que confeccionava, procurava me
               aprimorar nas técnicas. Hoje continuo me dividindo entre o CLT e o
               ATELIÊ, e buscando trazer em minhas peças um diferencial que
               atenda a necessidade dos meus clientes e também encante os olhos.
@@ -33,11 +31,11 @@ export default function About() {
               DIANA(Dianita costura criativa).
             </p>
             <a href="https://wa.me/5519983723718" target="_blank" rel="noreferrer">
-              <button className={styles.about__botaoSaibaMais}>
-                Saiba Mais
-              </button>
             </a>
           </div>
+          <button className={styles.about__botaoSaibaMais}>
+            Saiba Mais
+          </button>
         </div>
       </section>
     </>
