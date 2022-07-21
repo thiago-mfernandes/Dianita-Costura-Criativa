f36d90f917888aa6748ebc66955b8d1e3fef8b4f
Thiago Fernandes <falecomobranco@yahoo.com.br>
2022-07-21 11:17:02 -0300

    componente de formulario recebendo estilizacao

--- diana-costura-criativa/src/components/contact/index.tsx
+++ diana-costura-criativa/src/components/contact/index.tsx
@@ -1,35 +1,53 @@
-import styles from 'Contact.module.scss';
+import { FaInstagram, FaWhatsapp } from 'react-icons/fa';
+import { MdFacebook } from 'react-icons/md';
+import styles from './Contact.module.scss';
 
 export default function Contact() {
   return (
     <>
-      {/* contato */}
-      <section>
-        <div>
-          <h2>Contato</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic
-            suscipit repudiandae blanditiis at corporis, nam inventore eaque ea,
-            repellendus quia soluta porro aut adipisci recusandae dicta
-            assumenda, velit ipsam ex.
+      <section className={styles.container}>
+        
+        <div className={styles.boxTitle}>
+          <h2 className={styles.boxTitle__title}>Contato</h2>
+          <p className={styles.boxTitle__content}>
+            Para maiores informações sobre nossos produtos, preços e prazos de entrega, por favor informe seus dados no formulário abaixo e clique em enviar. Responderemos sua solicitação o mais breve possível.
           </p>
         </div>
-        <div>
-          <div>
-            <h3>Telefone</h3>
-            <p>00.00000.0000</p>
-            <h3>Email</h3>
-            <p>00.00000.0000</p>
-            <h3>Localização</h3>
-            <p>00.00000.0000</p>
-            <h3>Redes Sociais</h3>
-            <div>
-              <i>face</i>
-              <i>insta</i>
-              <i>whats</i>
-            </div>
+
+        
+        <div className={styles.boxMyInfo}>{/* caixa colorida */}
+          <h3 className={styles.boxMyInfo__title}>Telefone</h3>
+          <p className={styles.boxMyInfo__content}>55 19 983723718</p>
+          <h3 className={styles.boxMyInfo__title}>Email</h3>
+          <p className={styles.boxMyInfo__content}>dianita@costuracriativa.com</p>
+          <h3 className={styles.boxMyInfo__title}>Localização</h3>
+          <p className={styles.boxMyInfo__content}>Piracicaba - São Paulo - Brazil</p>
+          <h3 className={styles.boxMyInfo__title}>Redes Sociais</h3>
+          <div className={styles.boxSocialMedia}>
+            <a 
+              className={styles.boxSocialMedia__link}
+              href="https://www.facebook.com/profile.php?id=100063636849048"
+              target="_blank" rel="noreferrer"
+            >
+              <MdFacebook className={styles.boxSocialMedia__icon} />
+            </a>
+            <a 
+              className={styles.boxSocialMedia__link}
+              href="https://www.instagram.com/dianitacosturacriativa/"
+              target="_blank" rel="noreferrer"
+            >
+              <FaInstagram className={styles.boxSocialMedia__icon} />
+            </a>
+            <a 
+              className={styles.boxSocialMedia__link}
+              href="https://wa.me/5519983723718" 
+              target="_blank" rel="noreferrer"
+            >
+              <FaWhatsapp className={styles.boxSocialMedia__icon} />
+            </a>
           </div>
         </div>
+        
 
         <form action="">
           <p>
